634afe1ce2a503019ad22c66f76bc95d98483f76
ykz <ykz@gmail.com>
2022-09-27 21:58:45 +0800

    master commit

--- src/com/duoke360/Test.java
+++ src/com/duoke360/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("hello java v2");
         System.out.println("bug fix 我又修改了第7行");
         System.out.println("bug fix 我又修改了第8行");
+        System.out.println("测试推送到github....");
     }
 }
