d435b208496cb618e693fa2b37bfbf73fea705fa
ykz <ykz@gmail.com>
2022-09-27 17:46:28 +0800

    v2

--- src/com/duoke360/Test.java
+++ src/com/duoke360/Test.java
@@ -3,5 +3,6 @@ package com.duoke360;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello java v1");
+        System.out.println("hello java v2");
     }
 }
