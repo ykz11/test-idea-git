ef8d0dee49b362687411eaaf6244e1f8625d4909
ykz <ykz@gmail.com>
2022-09-27 19:01:06 +0800

    master commit

--- src/com/duoke360/Test.java
+++ src/com/duoke360/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello java v1");
         System.out.println("hello java v2");
-        System.out.println("bug fix modify...");
-        System.out.println("bug fix more...");
+        System.out.println("bug fix 我又修改了第7行");
+        System.out.println("bug fix 我又修改了第8行");
     }
 }
